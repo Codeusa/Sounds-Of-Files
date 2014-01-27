d5d9c5c4d70d6876fef7b2812c4365dc84bc69ee
Andrew Sampson <andrew@codeusa523.org>
2014-01-27 13:34:55 -0500

    Fixed byte value becoming negative, cleared up na
    Fixed byte value becoming negative, cleared up naming, assigned some underlying variables for future use, replaced the "Shout" method with just a simple logger.

--- sounds/SoundOfFiles.java
+++ sounds/SoundOfFiles.java
@@ -2,6 +2,7 @@ package sounds;
 
 import java.io.File;
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
@@ -9,57 +10,66 @@ import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
 
 public class SoundOfFiles {
+	private final static int CHANNELS = 1;
+	private final static boolean IS_BIG_ENDIAN = true;
+	private final static boolean IS_SIGNED = true;
+	private static final Logger logger = Logger.getLogger(SoundOfFiles.class
+			.getName());
+	private final static float SAMPLE_RATE = 44100.0f;
+	private final static int SAMPLE_SIZE = 8;
 
-	public static void getFileBytes(final File folder)
-			throws LineUnavailableException {
-		try {
-			for (final File fileEntry : folder.listFiles()) {
-				final long fileSizeInBytes = fileEntry.length();
-				if (fileEntry.isDirectory()) {
-					getFileBytes(fileEntry);
-				} else {
-					shout(fileEntry.getName());
-					playSound((int) fileSizeInBytes / 100); // saving your ears
-															// from
-															// death by / 100
-				}
-			}
-		} catch (final Exception e) {
-			shout("Invalid path entered"); // error handling at its finest.
-		}
+	public static void main(final String[] args) {
 
-	}
-
-	public static void main(final String[] args)
-			throws LineUnavailableException {
 		final String path;
 		if (args.length > 0) {
 			path = args[0];
 		} else {
 			final Scanner scanner = new Scanner(System.in);
-			shout("Enter the path to which you want to make sound (example C:/Files/):");
+			logger.info("Enter the path to which you want to make sound (example C:/Files/Project/):");
 			path = scanner.next(); // Get what the user types.
 			scanner.close();
 		}
 
-		final File folder = new File(path);
-		getFileBytes(folder);
+		try {
+			final File folder = new File(path);
+			playFile(folder);
+		} catch (final LineUnavailableException e) {
+			logger.severe("Encounter error: " + e.getClass());
+			logger.severe("Error message: " + e.getMessage());
+			logger.severe("Error cause: " + e.getCause());
+		}
+
 	}
 
-	private static void playSound(final int filesize)
+	public static void playFile(final File folder)
+			throws LineUnavailableException {
+		for (final File fileEntry : folder.listFiles()) {
+			if (fileEntry.isDirectory()) {
+				playFile(fileEntry);
+			} else {
+				logger.info("Playing File: "
+						+ String.valueOf(fileEntry.getName()));
+				final long fileSizeInBytes = fileEntry.length();
+				playSound(fileSizeInBytes);
+			}
+		}
+
+	}
+
+	private static void playSound(final long filesize)
 			throws LineUnavailableException {
 		final byte[] buffer = new byte[1];
 
-		final AudioFormat audioFormat = new AudioFormat(44100, 8, 1, true,
-				false);
+		final AudioFormat audioFormat = new AudioFormat(SAMPLE_RATE,
+				SAMPLE_SIZE, CHANNELS, IS_SIGNED, IS_BIG_ENDIAN);
 		SourceDataLine sourceDataLine = AudioSystem
 				.getSourceDataLine(audioFormat);
 		sourceDataLine = AudioSystem.getSourceDataLine(audioFormat);
 		sourceDataLine.open(audioFormat);
 		sourceDataLine.start();
-		for (int i = 0; i < 1000 * (float) 44100 / 1000; i++) {
-			final double angle = i / ((float) 44100 / filesize) * 2.0 * Math.PI;
-			buffer[0] = (byte) (Math.sin(angle) * 128); // mathmathmath
+		for (int i = 0; i < 1000 * SAMPLE_RATE / 1000; i++) {
+			final double angle = i / (SAMPLE_RATE / filesize) * 2.0 * Math.PI;
+			buffer[0] = (byte) (Math.sin(angle) * 127.0);
 			sourceDataLine.write(buffer, 0, 1);
 
 		}
@@ -67,10 +77,6 @@ public class SoundOfFiles {
 		sourceDataLine.drain();
 		sourceDataLine.stop();
 		sourceDataLine.close();
-	}
-
-	private static void shout(final String message) {
-		System.out.println(message);
 	}
 
 }
